4cf114edcd412c8d163ba1c295c7f4c03d9eebba
adluchessi <andre.luchessi@ufrn.br>
2023-07-11 09:22:58 -0300

    new scrip Elis by Andre

--- ggplot graficos ELIS.R
+++ ggplot graficos ELIS.R
@@ -1,50 +1,21 @@
 library(ggplot2)
 library(dplyr)
-iris
-data <- iris
-ggplot(data ,aes(x=Sepal.Width, y=Sepal.Length))+ giom_point()
-library(esquisse)
-install.packages("esquisse")
-library(esquisse)
-install.packages("dados")
-library(dados)
-dados_atmosfera
-dados <- dados_atmosfera
-dados |> esquisser()
-#############
-ggplot(dados) +
-  aes(x = nuvem_media, y = nuvem_alta, colour = ano) +
-  geom_point(shape = "circle", size = 1.5) +
-  scale_color_gradient() +
-  theme_minimal()
-####################
-install.packages("readxl")
-library(readxl)
-iris
-data <- iris
-library(esquisse)
-dados |> esquisser()
+#dataset
+names(iris)
 
-library(ggplot2)
+#plot
+ggplot(iris, aes(x=Sepal.Width, y=Sepal.Length))+
+  geom_point()+
+  theme()
 
-ggplot(dados) +
-  aes(x = Sepal.Length, y = Sepal.Width, colour = Species) +
-  geom_point(shape = "circle", size = 1.5) +
-  scale_color_hue(direction = 1) +
-  theme_minimal()
-?esquisse
-##############
-library(ggplot2)
-library(readxl)
-library(dplyr)
-install.packages("tidyverse")
-library(tidyverse)
-install.packages("readr")
-library(readr)
-read.csv()
-top50_atv_and_correspondente_ros.csv
-EmpSal <- read.csv('top50_atv_and_correspondente_ros.csv')
-top50_atv_and_correspondente_ros.csv
-read.csv(file, top50_atv_and_correspondente_ros.csv)
-setwd("C:\\Users\\Elis\\OneDrive\\Documentos\\BioInfoClass")
-read.csv("top50_atv_and_correspondente_ros.csv", header = TRUE
+#new dataset
+EmpSal <- read.csv('top50_atv_and_correspondente_ros.csv',
+                   sep = ";")
+head(EmpSal)
+
+#new plot
+p1=ggplot(EmpSal, aes(x=log2FC_ATV, y=pvalue_ATV))+
+  geom_point()+
+  theme(axis.title = element_text(size = 22,face = "bold"),
+        axis.text = element_blank())
+p1
